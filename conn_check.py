e708c63d61e404761226e0b7cd5d0949398bad3c
Joshua Abbey <Joshuaabbey2022@gmail.com>
2023-06-07 22:46:27 +0100

    rough prototype

--- conn_check.py
+++ conn_check.py
@@ -1,27 +1,57 @@
 import requests
-#import threading
+import threading
 import sites_db
+import time
 
-sites = sites_db.Database()
 
-def getStatusCode(url):
+class GetStatus():
+    def __init__(self):
+        self.lock = threading.Lock()
 
-    response = requests.head(url)
-    response_time = response.elapsed.total_seconds()
-    return response.status_code, response_time
-    
-def update_sites_db():
-    with sites as db:
-        urls = sites.get_urls()
-        urls = [i[0] for i in urls] # adds urls to a list
-        rows = sites.get_number_of_rows()
-    print(urls, rows)
-    with sites as db:
-        for x in range(rows):
-            status_code, response_time = getStatusCode(urls[x])
-            print(status_code, x)
-            sites.update_status_code(urls[x], status_code, response_time)
+    def getStatus(self, url):
+        response = requests.head(url)
+        response_time = response.elapsed.total_seconds()
+        return response.status_code, response_time
 
-update_sites_db()
-with sites as db:
-    print(sites.get_sites())
+    def updateDb(self):
+        try:
+            sites = sites_db.Database()
+            with sites as db:
+                urls = sites.get_urls()
+                urls = [i[0] for i in urls] 
+            def multiThreadRequest():
+                with sites as db:
+                    for url in urls:
+                        status_code, response_time = self.getStatus(url)
+                        #print(status_code, x)
+                        sites.update_status_code(urls[x], status_code, response_time)
+            def singleThreadRequest():
+                with sites as db:
+                    for url in urls:
+                            status_code, response_time = self.getStatus(url)
+                            print(status_code, url)
+                            sites.update_status_code(url, status_code, response_time)
+            return multiThreadRequest, singleThreadRequest
+        finally:
+            
+            print("process finished")
+
+    def run(self,multiThread=False, threadsCount=10):
+        multi, single = self.updateDb()
+        if not isinstance(multiThread, bool):
+            raise TypeError("multiThread arguement must be a boolean")
+        elif multiThread:
+            threads = []
+            for x in range(threadsCount):
+                print(f"thread {x+1} started")
+                time.sleep(5)
+                t = threading.Thread(target=multi)
+                threads.append(t) 
+                t.start()
+            for thread in threads:
+                thread.join()
+        else:
+            single()
+
+x=True
+GetStatus().run(True)
